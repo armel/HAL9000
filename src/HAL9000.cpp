4d5c1ebb6898811c47d7235931656c2697c5af70
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2021-12-25 06:52:49 +0100

    Add multi mode

--- src/HAL9000.cpp
+++ src/HAL9000.cpp
@@ -24,7 +24,8 @@
 #include <Arduino_GFX_Library.h>
 #include <SD.h>
 
-#define JPEG_FILENAME   "/HAL9000.jpg"
+#define JPEG_LOGO   "/HAL9000.jpg"
+#define JPEG_PANEL   "/HAL9000-panel.jpg"
 #define MJPEG_BUFFER_SIZE 32768 // memory for a single JPEG frame
 
 Arduino_DataBus *bus = new Arduino_ESP32SPI(27 /* DC */, 14 /* CS */, SCK, MOSI, MISO);
@@ -44,10 +45,19 @@ static unsigned long total_show_video = 0;
 static unsigned long start_ms, curr_ms;
 
 File root;
-String videoFilename[255];
-uint8_t indice = 0;
+String videoFilenameMedium[255];
+String videoFilenameSmall[255];
+
+uint8_t indiceMedium = 0;
+uint8_t indiceSmall = 0;
+uint8_t screen = 0;
 uint8_t current = 0;
 uint8_t last = 0;
+uint8_t mode = 0;
+uint8_t modeLast = 1;
+
+void medium();
+void small();
 
 // Pixel drawing callback
 static int mjpegDrawCallback(JPEGDRAW *pDraw)
@@ -79,8 +89,19 @@ void getVideoList(File dir)
 
     if(strstr(entry.name(), "/.") == NULL && strstr(entry.name(), ".mjpg") != NULL) {
       Serial.println(entry.name());
-      videoFilename[indice] = entry.name();
-      indice++;
+      if(strstr(entry.name(), "-medium") != NULL) {
+        videoFilenameMedium[indiceMedium] = entry.name();
+        indiceMedium++;
+        /*
+        if(indiceMedium==3) {
+          break;
+        }
+        */
+      }
+      else {
+        videoFilenameSmall[indiceSmall] = entry.name();
+        indiceSmall++;
+      }
     }
 
     if (entry.isDirectory()) {
@@ -105,6 +126,53 @@ void setup()
   gfx->fillScreen(BLACK);
   gfx->invertDisplay(true);
 
+  // Get video files
+  root = SD.open("/");
+  getVideoList(root);
+
+  // Multitasking task for retreive multi video
+  /*
+  xTaskCreatePinnedToCore(
+      process_1,    // Function to implement the task
+      "process_1",  // Name of the task
+      1024 * 32,    // Stack size in words
+      NULL,         // Task input parameter
+      1,            // Priority of the task
+      NULL,         // Task handle
+      0);           // Core where the task should run
+
+  xTaskCreatePinnedToCore(
+      process_2,    // Function to implement the task
+      "process_2",  // Name of the task
+      1024 * 32,    // Stack size in words
+      NULL,         // Task input parameter
+      1,            // Priority of the task
+      NULL,         // Task handle
+      1);           // Core where the task should run
+  */
+}
+
+boolean button()
+{
+  M5.update();
+
+  if (M5.BtnB.wasPressed()) {
+    return true;
+  }
+  return false;
+}
+
+void medium()
+{
+  File mjpegFile;
+
+  total_frames = 0;
+  total_read_video = 0;
+  total_decode_video = 0;
+  total_show_video = 0;
+
+  gfx->fillScreen(BLACK);
+
   if (!SD.begin())
   {
     Serial.println(F("ERROR: File System Mount Failed!"));
@@ -113,43 +181,25 @@ void setup()
   else
   {
     jpegClass.draw(&SD,
-        (char *)JPEG_FILENAME, jpegDrawCallback, true /* useBigEndian */,
+        (char *)JPEG_LOGO, jpegDrawCallback, true /* useBigEndian */,
         0 /* x */, 180 /* y */, gfx->width() /* widthLimit */, gfx->height() /* heightLimit */);
   }
-
-  // Get video files
-  root = SD.open("/");
-  getVideoList(root);
-
-  for(uint8_t i=0; i < indice; i++)
-  {
-    Serial.printf("%d -> ", i);
-    Serial.println(videoFilename[i]);
-  }
-}
-
-void loop()
-{
-  total_frames = 0;
-  total_read_video = 0;
-  total_decode_video = 0;
-  total_show_video = 0;
 
   while(1)
   {
     while(current == last) {
-      current = random(indice);      // Returns a pseudo-random integer between 0 and number of video files
+      current = random(indiceMedium);      // Returns a pseudo-random integer between 0 and number of video files
     }
 
-    Serial.println(videoFilename[current]);
+    Serial.println(videoFilenameMedium[current]);
 
-    File mjpegFile = SD.open(videoFilename[current], "r");
+    mjpegFile = SD.open(videoFilenameMedium[current], "r");
     uint8_t *mjpegBuf = (uint8_t *)malloc(MJPEG_BUFFER_SIZE);
 
     if (!mjpegFile || mjpegFile.isDirectory())
     {
       Serial.print("ERROR: Failed to open ");
-      Serial.print(videoFilename[current]);
+      Serial.print(videoFilenameMedium[current]);
       Serial.println(" file for reading");
     }
     else
@@ -164,25 +214,16 @@ void loop()
 
         start_ms = millis();
         curr_ms = millis();
-        mjpegClass.setup(
-            &mjpegFile, mjpegBuf, mjpegDrawCallback, true /* useBigEndian */,
-            0 /* x */, 0 /* y */, gfx->width() /* widthLimit */, gfx->height() /* heightLimit */);
+        mjpegClass.setup(&mjpegFile, mjpegBuf, mjpegDrawCallback, true, 0, 0, 320, 180);
 
         while (mjpegFile.available())
         {
-          M5.update();
-
-          if (M5.BtnB.isPressed()) {
-            for (uint8_t z = 0; z <= 128; z++) {
-              mjpegClass.readMjpegBuf();
-              total_read_video += millis() - curr_ms;
-              curr_ms = millis();
-              total_decode_video += millis() - curr_ms;
-              total_frames++;
-            }
-            Serial.printf("Jump Frame %d \n", total_frames);
+          if(button() == true)
+          {
+            mjpegFile.close(); 
+            free(mjpegBuf);
+            return;
           }
-
           // Read video
           mjpegClass.readMjpegBuf();
           total_read_video += millis() - curr_ms;
@@ -190,26 +231,127 @@ void loop()
 
           // Play video
           mjpegClass.drawJpg();
-          total_decode_video += millis() - curr_ms;
 
+          total_decode_video += millis() - curr_ms;
           curr_ms = millis();
           total_frames++;
         }
         int time_used = millis() - start_ms;
         Serial.println(F("MJPEG end"));
-        mjpegFile.close();
-        float fps = 1000.0 * total_frames / time_used;
-        total_decode_video -= total_show_video;
-        Serial.printf("Total frames: %d\n", total_frames);
-        Serial.printf("Time used: %d ms\n", time_used);
-        Serial.printf("Average FPS: %0.1f\n", fps);
-        Serial.printf("Read MJPEG: %lu ms (%0.1f %%)\n", total_read_video, 100.0 * total_read_video / time_used);
-        Serial.printf("Decode video: %lu ms (%0.1f %%)\n", total_decode_video, 100.0 * total_decode_video / time_used);
-        Serial.printf("Show video: %lu ms (%0.1f %%)\n", total_show_video, 100.0 * total_show_video / time_used);
-        
+        mjpegFile.close();        
         free(mjpegBuf);
       }
     }
+    
     last = current;
   }
+}
+
+void small()
+{
+  File mjpegFile;
+
+  uint16_t x = 0;
+  uint16_t y = 0;
+  
+  total_frames = 0;
+  total_read_video = 0;
+  total_decode_video = 0;
+  total_show_video = 0;
+
+  gfx->fillScreen(BLACK);
+
+  if (!SD.begin())
+  {
+    Serial.println(F("ERROR: File System Mount Failed!"));
+    gfx->println(F("ERROR: File System Mount Failed!"));
+  }
+  else
+  {
+    jpegClass.draw(&SD,
+        (char *)JPEG_PANEL, jpegDrawCallback, true /* useBigEndian */,
+        119 /* x */, 0 /* y */, gfx->width() /* widthLimit */, gfx->height() /* heightLimit */);
+  }
+
+  M5.Lcd.fillRect(110, 0, 9, 240, BLACK);
+  M5.Lcd.fillRect(201, 0, 9, 240, BLACK);
+
+  while(1)
+  {
+    screen = random(6);
+
+    if(screen > 2) {
+      x = 210;
+      y = 89 * (screen - 3);
+    }
+    else {
+      x = 0;
+      y = 89 * screen;
+    }
+
+    while(current == last) {
+      current = random(indiceSmall);      // Returns a pseudo-random integer between 0 and number of video files
+    }
+
+    Serial.println(videoFilenameSmall[current]);
+
+    mjpegFile = SD.open(videoFilenameSmall[current], "r");
+    uint8_t *mjpegBuf = (uint8_t *)malloc(MJPEG_BUFFER_SIZE);
+
+    if (!mjpegFile || mjpegFile.isDirectory())
+    {
+      Serial.print("ERROR: Failed to open ");
+      Serial.print(videoFilenameSmall[current]);
+      Serial.println(" file for reading");
+    }
+    else
+    {
+      if (!mjpegBuf)
+      {
+        Serial.println(F("mjpegBuf malloc failed!"));
+      }
+      else
+      {
+        Serial.println(F("MJPEG start"));
+
+        start_ms = millis();
+        curr_ms = millis();
+        mjpegClass.setup(&mjpegFile, mjpegBuf, mjpegDrawCallback, true, x, y, 110, 62);
+
+        while (mjpegFile.available())
+        {
+          if(button() == true)
+          {
+            mjpegFile.close(); 
+            free(mjpegBuf);
+            return;
+          }
+          // Read video
+          mjpegClass.readMjpegBuf();
+          total_read_video += millis() - curr_ms;
+          curr_ms = millis();
+
+          // Play video
+          mjpegClass.drawJpg();
+          gfx->fillRect(110, y, 9, 62, BLACK);
+
+          total_decode_video += millis() - curr_ms;
+          curr_ms = millis();
+          total_frames++;
+        }
+        int time_used = millis() - start_ms;
+        Serial.println(F("MJPEG end"));
+        mjpegFile.close();        
+        free(mjpegBuf);
+      }
+    }
+    
+    last = current;
+  }
+}
+
+void loop()
+{
+  medium();
+  small();
 }
